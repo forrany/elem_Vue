ad79c88e49cef276437579da3240d5e631f525c9
高闻灿[产品技术中心] <vincent.gao@corp.vipshop.com>
2018-06-14 18:17:43 +0800

    First Try config has been done and some init work has finished!

---
+++ sell/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I'm content
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import header from './components/header/Header'
+export default {
+  name: 'App',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    .tab
+      display flex
+      width 100%
+      height 2.5rem
+      line-height 2.5rem
+      .tab-item
+        flex 1
+        text-align center
+</style>
---
+++ sell/src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">
+        我是header
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+
+</style>
